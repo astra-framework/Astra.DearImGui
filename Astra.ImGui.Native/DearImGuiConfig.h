b4dfa5166e201bee00c31cb56e6368d8ccdf8d8f
atomsk-0 <wexberkyhd@gmail.com>
2024-11-18 16:08:44 +0200

    Fixed error C2159 when win32 backend added

--- Astra.ImGui.Native/DearImGuiConfig.h
+++ Astra.ImGui.Native/DearImGuiConfig.h
@@ -1,9 +1,11 @@
 ﻿#pragma once
+
 #ifdef _WIN32
 #define IMGUI_API __declspec(dllexport)
-#define IMGUI_IMPL_API extern "C" __declspec(dllexport)
+#define IMGUI_IMPL_API __declspec(dllexport) // Removed 'extern "C"' from here to fix error C2159: more than one storage class specified
 #else
 #define IMGUI_API
 #define IMGUI_IMPL_API extern "C"
 #endif
+
 #define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
